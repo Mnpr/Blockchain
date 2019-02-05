381cd6e9ed281ebc46b586544d92828cc00bcd8a
mnpr <achary.sudesh366@gmail.com>
2019-02-05 17:23:49 +0100

    bodyparser for server response

--- src/api.js
+++ src/api.js
@@ -2,16 +2,29 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/blockchain', function (req, res) {
-    res.send('Blockchain_info')
-  });
+const bodyParser = require('body-parser');
 
-app.post('/transaction', function (req, res){
-    res.send('Ttansactions')
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.post('/transaction', function (req, res){
+//     res.send(`The transaction amount ${req.body.amount} will transfer to ${req.body.reciever} `);
+// });
+ 
+var jsonParser = bodyParser.json();
+app.post('/transaction', jsonParser, function (req, res) {
+  if (!req.body) return res.sendStatus(400)
+  res.send(`The transaction amount ${req.body.amount} will transfer to ${req.body.receiver} `);
 });
 
+
+app.get('/blockchain', function (req, res) {
+    
+});
+
+
+
 app.get('/mine', function(req, res){
-    res.send('mining')
+    
 });
 
 app.listen(port, () => console.log(`listening on port ${port} !`));
